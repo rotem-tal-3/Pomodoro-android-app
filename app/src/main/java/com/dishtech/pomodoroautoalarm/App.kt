964e12771b646442c2ec8a48a680b106823cb2b8
rotem <psyberryd@gmail.com>
2025-08-13 09:45:02 +0300

    App: create notification channel on start up.

--- app/src/main/java/com/dishtech/pomodoroautoalarm/App.kt
+++ app/src/main/java/com/dishtech/pomodoroautoalarm/App.kt
@@ -1,6 +1,10 @@
 package com.dishtech.pomodoroautoalarm
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import com.dishtech.pomodoroautoalarm.AlarmWorker.Companion.CHANNEL_ID
 
 class App : Application() {
 
@@ -11,5 +15,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         appLifecycleObserver = AppLifecycleObserver(this)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                NotificationManager
+        val channel = NotificationChannel(CHANNEL_ID, "Alarm Notifications",
+                                          NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(channel)
     }
 }
