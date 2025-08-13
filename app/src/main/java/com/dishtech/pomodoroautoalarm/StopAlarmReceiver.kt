1681e603659baf356448c3462e9c7f32fe463402
rotem <psyberryd@gmail.com>
2025-08-13 09:44:27 +0300

    StopAlarmReceiver: Switch to alarm worker.

--- app/src/main/java/com/dishtech/pomodoroautoalarm/StopAlarmReceiver.kt
+++ app/src/main/java/com/dishtech/pomodoroautoalarm/StopAlarmReceiver.kt
@@ -1,16 +1,20 @@
 package com.dishtech.pomodoroautoalarm
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.work.WorkManager
 
 /**
  * A class to mitigate the foreground stop alarm service to the app.
  */
 class StopAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val serviceIntent = Intent(context, AlarmService::class.java)
-        context.stopService(serviceIntent)
+        WorkManager.getInstance(context.applicationContext)
+            .cancelAllWorkByTag(AlarmReceiver.POMODORO_ALARM_WORK_TAG)
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(AlarmWorker.NOTIFICATION_ID)
     }
 
 }
